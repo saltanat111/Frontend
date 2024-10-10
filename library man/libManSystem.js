a44a34ec978fd18fda61b5ab83581b6b69ec5fab
Saltanat Kydykbekova <saltanat.kydykbekova1@gmail.com>
2024-10-10 15:35:45 +0600

    commit for github

---
+++ library man/libManSystem.js
@@ -0,0 +1,38 @@
+const books = [
+    { title: "War and Peace", author: "Leo Tolstoy", isAvailable: true },
+    { title: "Crime and Punishment", author: "Fyodor Dostoevsky", isAvailable: false },
+    { title: "The Master and Margarita", author: "Mikhail Bulgakov", isAvailable: true },
+    { title: "The Idiot", author: "Fyodor Dostoevsky", isAvailable: false }
+  ];
+  
+  function getBooksByAuthor(books, author) {
+    return books
+      .filter(book => book.author === author)  
+      .map(book => book.title);
+  }
+  
+  function countAvailableBooks(books) {
+    return books
+      .filter(book => book.isAvailable)  
+      .length; 
+  }
+  
+  function lendBook(books, title) {
+    const book = books.find(book => book.title === title);  
+    
+    if (book) {  
+      if (book.isAvailable) {
+        book.isAvailable = false;  
+        return `The book ${title} has been successfully lent!`;
+      } else {
+        return `The book ${title} is not available!`;
+      }
+    } else {
+      return `The book ${title} was not found in the library!`;  
+    }
+  }
+  
+  console.log(getBooksByAuthor(books, "Fyodor Dostoevsky"));
+  console.log(countAvailableBooks(books));
+  console.log(lendBook(books, "War and Peace"));
+  console.log(lendBook(books, "War and Peace"));
